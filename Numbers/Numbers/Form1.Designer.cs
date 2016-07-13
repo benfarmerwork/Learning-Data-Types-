280cd858c009c28b57dc8afee9f10aad2dabd4b5
Ben Farmer <ben_farmer3@hotmail.com>
2016-07-14 00:01:00 +0100

    Final

---
+++ Numbers/Numbers/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnintegers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnintegers
+            // 
+            this.btnintegers.Location = new System.Drawing.Point(110, 20);
+            this.btnintegers.Name = "btnintegers";
+            this.btnintegers.Size = new System.Drawing.Size(75, 23);
+            this.btnintegers.TabIndex = 0;
+            this.btnintegers.Text = "Intergers";
+            this.btnintegers.UseVisualStyleBackColor = true;
+            this.btnintegers.Click += new System.EventHandler(this.btnintegers_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnintegers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnintegers;
+    }
+}
+
---
+++ Numbers/Numbers/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnintegers_Click(object sender, EventArgs e)
+        {
+            float firsNumber;
+            float secondNumber;
+            float floatAnswer;
+            firsNumber = 10.4F;
+            secondNumber = 11.2F;
+            floatAnswer = firsNumber + secondNumber;
+            MessageBox.Show(floatAnswer.ToString());
+        }
+    }
+}
